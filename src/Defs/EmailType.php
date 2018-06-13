0932b8a8fd3a35830a1d343679bb12f949459d1d
Guilherme Mattioli <guilherme@socarrao.com.br>
2018-06-13 15:49:52 -0300

    adicionado novos tipos de conexão

--- src/Defs/EmailType.php
+++ src/Defs/EmailType.php
@@ -11,6 +11,8 @@ class EmailType extends EmailDef {
 	const NONE = null;
 	const POP = 'pop3';
 	const POP_SSL = 'pop3/ssl';
+	const POP_SSL_NOVALIDATE = 'pop3/ssl/novalidate-cert';
 	const IMAP = 'imap';
 	const IMAP_SSL = 'imap/ssl';
+	const IMAP_SSL_NOVALIDATE = 'imap/ssl/novalidate-cert';
 }
